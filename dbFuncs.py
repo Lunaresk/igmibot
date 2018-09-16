d13acc8a6aa7ddc06d8e27bea7ed08aef559b401
Lunaresk <lingsonb@gmail.com>
2018-09-16 22:05:56 +0200

    Switch Registration To ReplyKeyboard
    Changed the InlineKeyboard from step one to ReplyKeyboard for better
    overview. Also, some warnings get erased this way.

--- dbFuncs.py
+++ dbFuncs.py
@@ -2,9 +2,11 @@ from ..bottoken import getConn
 from openpyxl import (Workbook, load_workbook)
 from datetime import date
 
+# Arrays for openpyxl
 months = ["Januar", "Februar", "März", "April", "Mai", "Juni", "Juli", "August", "September", "Oktober", "November", "Dezember"]
 heads = ["Vorname", "Nachname", "Email-Adresse", "Kaffee/Tee", "Softgetränke", "Wasser", "Schokolade", "Bier", "Alter Betrag", "Bezahlt", "Neuer Betrag"]
 
+# Initialize Database if not already initialized
 def initDB():
   with getConn('igmibot') as conn:
     cur = conn.cursor()
@@ -14,12 +16,14 @@ def initDB():
     cur.execute("CREATE TABLE IF NOT EXISTS Bill(Email TEXT PRIMARY KEY NOT NULL, Coffee INTEGER NOT NULL DEFAULT 0, Softdrink INTEGER NOT NULL DEFAULT 0, Water INTEGER NOT NULL DEFAULT 0, Choc INTEGER NOT NULL DEFAULT 0, Beer INTEGER NOT NULL DEFAULT 0, Oldbill INTEGER NOT NULL DEFAULT 0, Paid INTEGER NOT NULL DEFAULT 0);")
     conn.commit()
 
+# Connect a telegram ID with the IGMI Mail
 def insertMember(email, lang, tid, message):
   with getConn('igmibot') as conn:
     cur = conn.cursor()
     cur.execute("INSERT INTO TelegramID(Id, EMail, Message, Language) VALUES(%s, %s, %s, %s);", (tid, email, message, lang))
     conn.commit()
 
+# Check if the entered email-adress is registered in the database
 def isIGMIMember(email):
   with getConn('igmibot') as conn:
     cur = conn.cursor()
@@ -28,6 +32,7 @@ def isIGMIMember(email):
       return True
     return False
 
+# Check if Telegram ID or email-adress is already connected in the TelegramID Table
 def isTGMember(tid = 0, email = ""):
   with getConn('igmibot') as conn:
     cur = conn.cursor()
@@ -36,62 +41,75 @@ def isTGMember(tid = 0, email = ""):
       return True
     return False
 
+# Get an IGMI Member via his email
 def getMemberPerMail(email):
   with getConn('igmibot') as conn:
     cur = conn.cursor()
     cur.execute("SELECT * FROM Members WHERE EMail = %s;", (email,))
     return cur.fetchone()
 
+# Get an IGMI Member via the Telegram ID
 def getMemberPerID(tid):
   with getConn('igmibot') as conn:
     cur = conn.cursor()
     cur.execute("SELECT EMail FROM TelegramID WHERE Id = %s;", (tid,))
     return getMemberPerMail(cur.fetchone()[0])
 
+# Get the data from a specific person from the TelegramID Table
 def getTelegramID(id = 0, mail = ""):
   with getConn('igmibot') as conn:
     cur = conn.cursor()
     cur.execute("SELECT * FROM TelegramID WHERE Id = %s OR Email = %s;", (id, mail))
     return cur.fetchone()
 
+# Get every connected Telegram ID
 def getTelegramIDList():
   with getConn('igmibot') as conn:
     cur = conn.cursor()
     cur.execute("SELECT Id FROM TelegramID;")
     return cur.fetchall()
 
+# Get the Bill datas from a specific member
 def getBill(tid):
   with getConn('igmibot') as conn:
     cur = conn.cursor()
     cur.execute("SELECT * FROM Bill WHERE Email = (SELECT Email FROM TelegramID WHERE Id = %s);", (tid,))
     return cur.fetchone()
 
+# When a new message had to be sent, the message-id is stored here
 def updateMessage(id, message):
   with getConn('igmibot') as conn:
     cur = conn.cursor()
     cur.execute("UPDATE TelegramID SET Message = %s WHERE Id = %s;", (message, id))
     conn.commit()
 
+# When the chosen language changed, the data in the database is updated
 def updateLanguage(id, lang):
   with getConn('igmibot') as conn:
     cur = conn.cursor()
     cur.execute("UPDATE TelegramID SET Language = %s WHERE Id = %s;", (lang, id))
     conn.commit()
 
+# Updating the bill data when someone bought something
 def updateBill(tid, coffee = 0, soft = 0, water = 0, choc = 0, beer = 0):
   with getConn('igmibot') as conn:
     cur = conn.cursor()
     cur.execute("UPDATE Bill SET Coffee = Coffee+%s, Softdrink = Softdrink+%s, Water = Water+%s, Choc = Choc+%s, Beer = Beer+%s WHERE Email = (SELECT Email FROM TelegramID WHERE Id = %s);", (coffee, soft, water, choc, beer, tid))
     conn.commit()
 
+# Create Excel sheets for IGMI
+# Called every 1st of the month at 12:00
 def createExcel(excelDir):
-  thisYear = date.today().year
-  thisMonth = date.today().month
+  today = date.today()
+  thisYear, thisMonth = [today.year, today.month]
+  # While we have the first of the next month, we have to reduce it by one for our calculations
   if thisMonth == 1:
     thisYear -= 1
     thisMonth = 12
   else:
     thisMonth -= 1
+  # Try to load the recent workbook. If that fails, create a new one
+  # wb = workbook, ws = worksheet
   try:
     wb = load_workbook("{0}/{1}.xlsx".format(excelDir, thisYear))
     ws = wb.create_sheet(months[thisMonth-1])
@@ -101,16 +119,21 @@ def createExcel(excelDir):
     ws.title = months[thisMonth-1]
   ws.append(heads)
   ws.append([""])
+  # Fetch datas from the Bill and Members table to fill the worksheet
   with getConn('igmibot') as conn:
     cur = conn.cursor()
     cur.execute("SELECT M.firstname, M.lastname, M.Email, B.Coffee, B.Softdrink, B.Water, B.Choc, B.Beer, B.Oldbill/100, B.Paid/100 FROM Members M, Bill B WHERE M.email = B.email ORDER BY M.lastname, M.firstname, M.email ASC;")
+    # Processing one entry at a time to keep RAM clean
     iterator = cur.fetchone()
     while iterator:
       ws.append(list(iterator) + ["=(D{0}*30 + E{0}*100 + F{0}*60 + G{0}*25 + H{0}*100 + I{0}*100 - J{0}*100)/100".format(ws.max_row+1)])
       iterator = cur.fetchone()
+  # Set the column width to proper size
   for column in ws.columns:
+    # The middle ones (Schokolade, Softgetränke etc.) get a fixed size
     if column[0].value in heads[3:8]:
       ws.column_dimensions[column[0].column].width = 13
+    # The other's sizes get calculated
     else:
       length = max(len(str(cell.value)) for cell in column_cells)
       ws.column_dimensions[column_cells[0].column].width = length
@@ -118,12 +141,15 @@ def createExcel(excelDir):
   wb.save("{0}/{1}.xlsx".format(excelDir, thisYear))
   calculateBill()
 
+# Function for end of the month. Summarizes the bought wares and calculates the price.
+# Then sets the bought wares to zero again. All prices in cent.
 def calculateBill():
   with getConn('igmibot') as conn:
     cur = conn.cursor()
     cur.execute("UPDATE Bill SET Oldbill = (Oldbill + Coffee*30 + Softdrink*100 + Water*60 + Choc*25 + Beer*100 - Paid), Coffee = 0, Softdrink = 0, Water = 0, Choc = 0, Beer = 0, Paid = 0;")
     conn.commit()
 
+# Removes the Email-Telegram Connection
 def removeTelegramID(id = 0, mail = ""):
   with getConn('igmibot') as conn:
     cur = conn.cursor()
--- helpFuncs.py
+++ helpFuncs.py
@@ -13,20 +13,26 @@ logger = logging.getLogger(__name__)
 def createCode():
   return str(uuid4())
 
+# Function for sending a mail
 def sendMail(sender, receiver, message):
+  #MIME for easy formatting email content
   msg = MIMEMultipart()
+  # From, To, Subject defines the header
   msg['From'] = sender
   msg['To'] = ', '.join(receiver)
   msg['Subject'] = Header(message.split("\n", 1)[0], 'utf-8')
+  # Attach meesage content. 'message' has to be split at the first \n occurence
+  # to separate header from content
   msg.attach(MIMEText(message.split("\n", 1)[1].encode('utf-8'), 'plain', 'utf-8'))
   with getMail('igmibot') as smtp:
-    try:
+    try: # try to send the mail
       smtp.sendmail(sender, receiver, msg.as_string())
       return True
     except Exception as error:
       logger.critical(repr(error))
       return False
 
+# Function to evaluate if an object can be cast to int
 def isInt(number):
   try:
     int(number)
@@ -34,6 +40,7 @@ def isInt(number):
   except ValueError:
     return False
 
+# Function to get the next month. Returns datetime object
 def nextReminder():
   today = datetime.date.today()
   thisYear, thisMonth = [today.year, today.month]
@@ -42,6 +49,9 @@ def nextReminder():
     thisYear += 1
   return datetime.datetime(thisYear, thisMonth+1, 1, 12, 0, 0)
 
+
+# Just a test function, now deprecated
+# Maybe I need it again for testing purposes
 def testReminder():
   now = datetime.datetime.now()
   year, month, day, hour, minute = [now.year, now.month, now.day, now.hour, now.minute]
